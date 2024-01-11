20192dc814418a2b530314aa78bdd6fb00ac08b5
Fabrizio Lanzillo <fabriziolanzillo@gmail.com>
2024-01-11 20:31:19 +0100

    login type error fix

--- src/php/login.php
+++ src/php/login.php
@@ -91,7 +91,7 @@ if(checkFormData(['email', 'password'])){
     } else {
         try {
             // Protect against XSS
-            $email = htmlspecialchars($_POST['eamil'], ENT_QUOTES, 'UTF-8');
+            $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
             $rawPassword = htmlspecialchars($_POST['password'], ENT_QUOTES, 'UTF-8');
             // retrieve from the db the salt of the user
             $result = getAccessInformation($email);
